b1f85a41a17cffa239634459874f3a4bc1b69cfe
kelso0n <kelsonsobrinho2016@gmail.com>
2025-06-18 10:33:15 -0300

    Update CartasSuperTrunfo kelson

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,11 @@
+Carta 1
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main() {" população da cidade é %s\n" , "1.774 milhão"}
+ 
+printf("O nome da cidade é %s\n", "Curitiba");
 
-int main() {
+int main() {" população da cidade é %s\n" , "2,4 milhões "}
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
